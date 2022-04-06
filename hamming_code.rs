9b99f8f101677a090e515b7e424df13a1270ac88
binotation <68807919+binotation@users.noreply.github.com>
2022-04-06 15:03:29 +1000

    Allow decoding to determine error type and added more tests

--- hamming_code.rs
+++ hamming_code.rs
@@ -1,3 +1,11 @@
+#[derive(Debug, PartialEq)]
+enum ErrorType {
+    NoError,
+    SingleBitError,
+    DoubleBitError,
+    ParityBitError,
+}
+
 /// Encode n using hamming(8, 4).
 ///     h 2 1 0 d 3 2 1 0   d
 /// G = [ 0 1 1 | 1 0 0 0   3
@@ -29,7 +37,7 @@ fn hamming_encode(n: u8) -> u8 {
 /// s = Hx: (s0 s1 s2)^T
 /// returns error-corrected data bits
 /// i.e. error-corrected x4 x3 x2 x1
-fn hamming_decode(mut x: u8) -> u8 {
+fn hamming_decode(x: u8) -> (u8, ErrorType) {
     // Calculate syndrome bits using H
     let s: [u8; 3] = [
         (x >> 7 & 1) ^ (x >> 3 & 1) ^ (x >> 2 & 1) ^ (x >> 1 & 1),
@@ -43,33 +51,118 @@ fn hamming_decode(mut x: u8) -> u8 {
     // Map syndrome to incorrect bit position: 7 -> 1, 6 -> 2, 5 -> 3, 3 -> 4, 1 -> 5, 2 -> 6, 4 -> 7
     // e.g. if syndrome = 4 then flip bit x7.
     const SYNDROME_TO_BIT: [usize; 8] = [0, 5, 6, 4, 7, 3, 2, 1];
-    x = x ^ 1 << SYNDROME_TO_BIT[syndrome];
+    let incorrect_bit = SYNDROME_TO_BIT[syndrome];
+    let data = (x ^ 1 << incorrect_bit) >> 1 & 0xF; // Flip incorrect bit and return data bits
+    let x_parity = (x >> 7 & 1)
+        ^ (x >> 6 & 1)
+        ^ (x >> 5 & 1)
+        ^ (x >> 4 & 1)
+        ^ (x >> 3 & 1)
+        ^ (x >> 2 & 1)
+        ^ (x >> 1 & 1);
+    let error_type;
 
-    x >> 1 & 0xF // Throw away parity bit
+    if x & 1 != x_parity {
+        if incorrect_bit > 0 {
+            error_type = ErrorType::SingleBitError;
+        } else {
+            error_type = ErrorType::ParityBitError;
+        }
+    } else {
+        if incorrect_bit == 0 {
+            error_type = ErrorType::NoError;
+        } else {
+            error_type = ErrorType::DoubleBitError;
+        }
+    }
+    (data, error_type)
 }
 
 fn main() {
-    let mut decoded: u8;
-    let n: [u8; 4] = [0b00001011, 0b00001000, 0b00001111, 0b00001010];
-
+    const N: [u8; 4] = [0b00001011, 0b00001000, 0b00001111, 0b00001010];
+    const EXPECTED_X: [u8; 4] = [0b01010110, 0b01110001, 0b11111111, 0b10110100];
     let x: [u8; 4] = [
-        hamming_encode(n[0]),
-        hamming_encode(n[1]),
-        hamming_encode(n[2]),
-        hamming_encode(n[3]),
+        hamming_encode(N[0]),
+        hamming_encode(N[1]),
+        hamming_encode(N[2]),
+        hamming_encode(N[3]),
     ];
 
-    assert_eq!(x[0], 0b01010110);
-    assert_eq!(x[1], 0b01110001);
-    assert_eq!(x[2], 0b11111111);
-    assert_eq!(x[3], 0b10110100);
-    println!("Encoded data matches expected");
-    for (i, c) in x.iter().enumerate() {
-        for j in 1..8 {
-            let errored = c ^ 1 << j;
+    test_expected_x(&x, &EXPECTED_X);
+    test_single_bit_or_parity_error(&x, &N);
+    test_no_error(&x, &N);
+    test_double_bit_error(&x);
+    println!("ALL TESTS PASSED!!!!!!!!");
+}
+
+fn test_double_bit_error(x: &[u8; 4]) {
+    let mut count;
+    let mut errored;
+    let mut decoded;
+    let mut e1;
+    let mut e2;
+    let mut e;
+
+    for c in x {
+        e1 = 1;
+        e2 = 2;
+        count = 0;
+
+        // 8 choose 2 = 28 combinations. Can't think of a better way to do this.
+        while e1 >> 6 & 1 == 0 {
+            while e2 >> 7 & 1 == 0 {
+                e = e1 | e2;
+                errored = *c ^ e;
+                decoded = hamming_decode(errored);
+                assert_eq!(decoded.1, ErrorType::DoubleBitError);
+                e2 <<= 1;
+                count = count + 1;
+            }
+            e1 <<= 1;
+            e2 = e1 << 1;
+        }
+        for i in 0..=6 {
+            e = e1 >> i | e2;
+            errored = *c ^ e;
             decoded = hamming_decode(errored);
-            assert_eq!(decoded, n[i]);
+            assert_eq!(decoded.1, ErrorType::DoubleBitError);
+            count = count + 1;
+        }
+        assert_eq!(count, 28); // Sanity check
+    }
+    println!("Double-bit errors were successfully detected");
+}
+
+fn test_no_error(x: &[u8; 4], n: &[u8; 4]) {
+    let mut decoded;
+    for (i, c) in x.iter().enumerate() {
+        decoded = hamming_decode(*c);
+        assert_eq!(decoded.0, n[i]);
+        assert_eq!(decoded.1, ErrorType::NoError);
+    }
+    println!("Data with no errors successfully decoded.");
+}
+
+fn test_single_bit_or_parity_error(x: &[u8; 4], n: &[u8; 4]) {
+    let mut decoded;
+    for (i, c) in x.iter().enumerate() {
+        for j in 0..8 {
+            let errored = *c ^ 1 << j;
+            decoded = hamming_decode(errored);
+            assert_eq!(decoded.0, n[i]);
+            if j == 0 {
+                assert_eq!(decoded.1, ErrorType::ParityBitError);
+            } else {
+                assert_eq!(decoded.1, ErrorType::SingleBitError);
+            }
         }
     }
-    println!("Decoded data was successfully error corrected")
+    println!("Single-bit/parity-bit errors were successfully error corrected");
+}
+
+fn test_expected_x(x: &[u8; 4], expected_x: &[u8; 4]) {
+    for (i, c) in x.iter().enumerate() {
+        assert_eq!(*c, expected_x[i]);
+    }
+    println!("Encoded data matches expected");
 }
